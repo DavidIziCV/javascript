9a61f8d1e5d66013e56cd0d8f5b6f569dfeb7e85
David Izidório <126904043+DavidIziCV@users.noreply.github.com>
2023-06-08 10:13:33 -0300

    update

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('olá!')
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12 ){
+        //Bom dia!
+        img.src = 'manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    }else if (hora>=12 && hora <18 ){
+        //Boa tarde!
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#b9846f'
+    }else {
+        //Boa Noite!
+        img.scr = 'noite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
