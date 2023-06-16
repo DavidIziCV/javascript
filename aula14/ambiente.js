3298462b9c37a0815c1f880713973e581c2a6b07
David Izidório <126904043+DavidIziCV@users.noreply.github.com>
2023-06-16 20:12:30 -0300

    aula14

---
+++ aula14/ambiente.js
@@ -0,0 +1,11 @@
+/*var cont = 1          inicialização
+do {
+    console.log(cont)
+    cont++              incremento
+} while (cont <= 6)     teste logico*/ 
+
+console.log('Vai começar!')
+for(var cont = 1;cont <=6; cont++){
+    console.log(cont)
+}
+console.log('FIM!')
